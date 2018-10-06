20aa289fb23881528d1378fd0e6d37df36fa56ce
yasu-lubuntu-vbox-galleria <you@example.com>
2018-10-06 16:04:48 +0900

    implement some opencv function

--- pyqtgraph-use/node_calc.py
+++ pyqtgraph-use/node_calc.py
@@ -1,4 +1,8 @@
+import copy
+import threading
+
 import cv2
+
 from PyQt5 import QtCore, QtGui, QtWidgets
 import pyqtgraph as pg
 import pyqtgraph.flowchart as pgfc
@@ -8,7 +12,7 @@ import my_pyqtgraph.flowchart.library.common as pgfclc
 import my_pyqtgraph as mypg
 
 
-class AbstractImageProcessNode(pgfclc.CtrlNode):
+class AbstractCalcNode(pgfclc.CtrlNode):
     """
     Abstract ImageProcess Node
     """
@@ -23,70 +27,148 @@ class AbstractImageProcessNode(pgfclc.CtrlNode):
                                        # as well..
         pgfclc.CtrlNode.__init__(self, name, terminals=terminals)
 
+    @staticmethod
+    def is_gray(data_in):
+        if   len(data_in['image'].shape)==2:
+            return True
+        else:
+            assert data_in['image'].shape[2] == 3
+            return False
 
-class CannyNode(AbstractImageProcessNode):
+
+class CannyNode(AbstractCalcNode):
     nodeName = "Canny"
     uiTemplate = [
         ('min', 'spin', {'value': 100, 'step': 1, 'bounds': [0, None], 'int':True }),
         ('max', 'spin', {'value': 200, 'step': 1, 'bounds': [0, None], 'int':True }),
     ]
     def process(self, data_in, display=True):
+        # should shallow copy soon because "data_in" will change maybe
+        data_in  = copy.copy(data_in)
+        data_out = data_in
+
         min = self.ctrls['min'].value()
         max = self.ctrls['max'].value()
-        data_out = data_in
+
         data_out['image'] = cv2.Canny(data_in['image'], min, max)
         return {'data_out': data_out}
 
 
-class CvtColorNode(AbstractImageProcessNode):
-    code_names = ['BGR', 'RGB', 'HSV', 'HLS', 'GRAY']
-    nodeName = "CvtColor"
-    uiTemplate = [
-        (
-            'type_in', 'combo',
-            {
-                'value': 'BGR',
-                'values': code_names
-            }
-        ),(
-            'type_out', 'combo',
-            {
-                'value': 'RGB',
-                'values': code_names
-            }
-        )
-    ]
+class CvtColorNode(AbstractCalcNode):
+    color_names = ['BGR', 'RGB', 'HSV', 'HLS', 'GRAY']
     cv2_cvt_codes = \
         [[None, cv2.COLOR_BGR2RGB, cv2.COLOR_BGR2HSV, cv2.COLOR_BGR2HLS, cv2.COLOR_BGR2GRAY],
          [cv2.COLOR_RGB2BGR, None, cv2.COLOR_RGB2HSV, cv2.COLOR_RGB2HLS, cv2.COLOR_RGB2GRAY],
          [cv2.COLOR_HSV2BGR, cv2.COLOR_HSV2RGB, None, None, None],
          [cv2.COLOR_HLS2BGR, cv2.COLOR_HLS2RGB, None, None, None],
          [cv2.COLOR_GRAY2BGR, cv2.COLOR_GRAY2RGB, None, None, None]]
+
+    nodeName = "CvtColor"
+    uiTemplate = [
+        ('type_in',  'combo', {'value': 'BGR', 'values': color_names}),
+        ('type_out', 'combo', {'value': 'RGB', 'values': color_names}),
+    ]
     
     def process(self, data_in, display=True):
+        # should shallow copy soon because "data_in" will change maybe
+        data_in  = copy.copy(data_in)
+        data_out = data_in
+
         idx_in  = self.ctrls['type_in' ].currentIndex()
         idx_out = self.ctrls['type_out'].currentIndex()
-        data_out = data_in
-        idx_in  = self.correct_index(idx_in, len(data_in['image'].shape)==2)
+
+        if self.is_gray(data_in):
+            idx_in = self.color_names.index('GRAY')
+        else:
+            if idx_in == self.color_names.index('GRAY'):
+                idx_in = self.color_names.index('BGR')
+        self.ctrls['type_in'].setCurrentIndex(idx_in)
+
         cv2_cvt_code = self.cv2_cvt_codes[idx_in][idx_out]
+
         if cv2_cvt_code is not None:
             data_out['image'] = cv2.cvtColor(data_in['image'], cv2_cvt_code)
+        else:
+            data_out['image'] = data_in['image']
         return {'data_out':data_out}
-    
-    @classmethod
-    def correct_index(cls, idx, is_gray):
-        """
-        if image is grayscale -> idx be corrected GRAY idx
-        if image is not grayscale and idx is grayscale -> idx be corrected RGB idx
-        """
-        if is_gray:
-            idx = cls.code_names.index('GRAY')
-        if not is_gray and idx == cls.code_names.index('GRAY'):
-            idx = cls.code_names.index('BGR')
-        return idx
 
 
-class UnsharpMaskNode(AbstractImageProcessNode):
+class ThreshNode(AbstractCalcNode):
+    thresh_names = \
+        ['BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV']
+    cv2_thresh_types = \
+        [cv2.THRESH_BINARY, cv2.THRESH_BINARY_INV, cv2.THRESH_TRUNC,
+         cv2.THRESH_TOZERO, cv2.THRESH_TOZERO_INV]
+
+    nodeName = 'Threshold'
+    uiTemplate = [
+        ('type',  'combo', {'value': 'BINARY', 'values': thresh_names}),
+        ('threshold', 'spin', {'value': 150, 'step': 1, 'bounds': [0, None], 'int':True }),
+        ('max_value', 'spin', {'value': 200, 'step': 1, 'bounds': [0, None], 'int':True }),
+        ('otsu', 'check', {'checkd': False}),
+    ]
+    def process(self, data_in, display=True):
+        # should shallow copy soon because "data_in" will change maybe
+        data_in = copy.copy(data_in)
+        data_out = data_in
+
+        thresh_idx = self.ctrls['type' ].currentIndex()
+        threshold = self.ctrls['threshold'].value()
+        max_value = self.ctrls['max_value'].value()
+        has_otsu = self.ctrls['otsu'].isChecked()
+        cv2_thresh_type = self.cv2_thresh_types[thresh_idx]
+
+        if has_otsu:
+            if self.is_gray(data_in):
+                threshold = 0
+                cv2_thresh_type = cv2_thresh_type + cv2.THRESH_OTSU
+            else:
+                return {'data_out':data_out}
+        
+        _, data_out['image'] = cv2.threshold(data_in['image'], threshold, max_value, cv2_thresh_type)
+        return {'data_out':data_out}
+
+
+class AdaptThreshNode(AbstractCalcNode):
+    thresh_names = \
+        ['BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV']
+    cv2_thresh_types = \
+        [cv2.THRESH_BINARY, cv2.THRESH_BINARY_INV, cv2.THRESH_TRUNC,
+         cv2.THRESH_TOZERO, cv2.THRESH_TOZERO_INV]
+    adapt_method_names = \
+        ['MEAN_C', 'GAUSSIAN_C']
+    cv2_adapt_methods = \
+        [cv2.ADAPTIVE_THRESH_MEAN_C, cv2.ADAPTIVE_THRESH_GAUSSIAN_C]
+
+    nodeName = 'AdaptiveThreshold'
+    uiTemplate = [
+        ('type',  'combo', {'value': 'BINARY', 'values': thresh_names}),
+        ('method',  'combo', {'value': 'BINARY', 'values': adapt_method_names}),
+        ('max_value', 'spin', {'value': 255, 'step': 1, 'bounds': [0, None], 'int':True }),
+        ('block_size', 'spin', {'value': 11, 'step': 1, 'bounds': [0, None], 'int':True }),
+        ('parameter', 'spin', {'value': 2, 'step': 1, 'bounds': [None, None] }),
+    ]
+    def process(self, data_in, display=True):
+        # should shallow copy soon because "data_in" will change maybe
+        data_in = copy.copy(data_in)
+        data_out = data_in
+
+        if not self.is_gray(data_in):
+            return {'data_out':data_out}
+
+        thresh_idx = self.ctrls['type' ].currentIndex()
+        method_idx = self.ctrls['method'].currentIndex()
+        mv = self.ctrls['max_value'].value()
+        bs = self.ctrls['block_size'].value()
+        p1 = self.ctrls['parameter'].value()
+        cv2_tt = self.cv2_thresh_types[thresh_idx]
+        cv2_am = self.cv2_adapt_methods[method_idx]
+        data_out['image'] = cv2.adaptiveThreshold(data_in['image'], mv, cv2_am, cv2_tt, bs, p1)
+
+        return {'data_out':data_out}
+
+
+class UnsharpMaskNode(AbstractCalcNode):
     """Return the input data passed through an unsharp mask."""
     nodeName = "UnsharpMask"
     uiTemplate = [
@@ -107,6 +189,8 @@ def add_library(library):
     if isinstance( library, pgfc.NodeLibrary.NodeLibrary ):
         library.addNodeType(CannyNode , [('Process',),])
         library.addNodeType(CvtColorNode , [('Process',),])
+        library.addNodeType(ThreshNode , [('Process',),])
+        library.addNodeType(AdaptThreshNode , [('Process',),])
         library.addNodeType(UnsharpMaskNode , [('Image',), 
                                               ('Submenu_test','submenu2','submenu3')])
     return library
